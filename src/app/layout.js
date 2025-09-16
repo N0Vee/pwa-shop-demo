1c1d8cd77a6a0901f463b5677a2eec37eea52b1c
N0Vee <wanidchanon@gmail.com>
2025-09-16 14:42:00 +0700

    feat: Add more Image

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.jpg" />
       </head>
       <body className="bg-gray-50">
         <CartProvider>{children}</CartProvider>
